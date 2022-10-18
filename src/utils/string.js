ada454bd809a539a55f70b34956af303112dc5aa
qingyili618 <59698058+qingyili618@users.noreply.github.com>
2022-10-18 15:26:49 +0800

    Add function I

--- src/utils/string.js
+++ src/utils/string.js
@@ -7,3 +7,13 @@ export function T (template, data) {
   }
   return res
 }
+
+export function I (template, choice) {
+  let res = template
+  const regexp = /\[\[[^\]]+\]\]/g
+  while (regexp.test(res)) {
+    const keys = res.match(regexp).map(x => x.substring(2, x.length - 2))
+    for (const k of keys ) res = res.replaceAll('[[' + k + ']]', choice? k.split('|')[0] : k.split('|')[1])
+  }
+  return res
+}
