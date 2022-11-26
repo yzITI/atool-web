bd2de399915133a59953e01b416de0445f2f035a
Phantomlsh <phantomlsh@outlook.com>
2022-11-26 11:46:13 -0800

    Sashimi service (#4)
    * ui
    
    * Update Service.vue
    
    * Update Service.vue
    
    * formSelectMenu
    
    * submit bug
    
    submit data.form -> data.forms
    
    * Basic service page
    
    Co-authored-by: Layzzi <33226552+SiuGaaLei@users.noreply.github.com>

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -4,16 +4,16 @@ import { I } from '../utils/string.js'
 import Toggle from '../components/Toggle.vue'
 import srpc from '../utils/srpc.js'
 import Editor from '../components/Editor.vue'
-import { DocumentTextIcon, Bars3Icon } from '@heroicons/vue/24/outline'
+import { DocumentTextIcon, Bars3Icon, PencilSquareIcon, XMarkIcon, PlusIcon, CodeBracketIcon, CheckIcon } from '@heroicons/vue/24/outline'
 import { useRouter, useRoute } from 'vue-router'
 const router = useRouter(), route = useRoute()
 
 const nid = route.params.id
-let editable = $computed(() => state.nodes[nid]?.role === 'editor' || state.nodes[nid]?.role === 'owner')
+let isAdmin = $computed(() => state.nodes[nid]?.role === 'owner')
 
 init()
 
-let info = $ref({}), form = $ref([]), codes = $ref({})
+let info = $ref({}), forms = $ref({}), codes = $ref({}), coding = $ref(false), keyword = $ref(''), queryInput = $ref('')
 
 let timeStr = $computed(() => {
   if (!info.time) return ''
@@ -27,6 +27,11 @@ function refreshLink () {
   }
 }
 
+function choose (id) {
+  if (forms[id]) return delete forms[id]
+  forms[id] = { title: state.nodes[id].data.title }
+}
+
 async function init () {
   state.loading = true
   const res = await srpc.node.get(state.user?.token || '', nid)
@@ -36,8 +41,8 @@ async function init () {
   }
   state.loading = false
   info = { title: res.title, time: res.time, public: res.public }
-  form = JSON.parse(res.form || '[]')
-  for (const f of form) {
+  forms = JSON.parse(res.forms || '{}')
+  for (const f in forms) {
     if (res[f]) codes[f] = res[f]
   }
   refreshLink()
@@ -45,8 +50,8 @@ async function init () {
 
 async function submit () {
   if (!state.user?.token) return
-  const data = { title: info.title, public: info.public, type: 'service', form: JSON.stringify(form) }
-  for (const f of form) {
+  const data = { title: info.title, public: info.public, type: 'service', forms: JSON.stringify(forms) }
+  for (const f in forms) {
     if (codes[f]) data[f] = codes[f]
   }
   state.loading = true
@@ -56,13 +61,56 @@ async function submit () {
   refreshLink()
   Swal.fire(I('[[Success|保存成功]]'), '', 'success')
 }
+
+let showForm = $ref(false)
 </script>
 
 <template>
+  <Transition name="fade">
+    <div class="fixed top-0 left-0 w-screen h-screen bg-black opacity-50" v-if="coding" @click="coding = false" />
+  </Transition>
+  <div class="all-transition fixed p-2 w-11/12 md:w-2/3 bg-white h-screen" :class="coding ? 'right-0' : '-right-full'">
+    <div class="text-sm text-gray-500 mb-2">{{ I('[[Backend Code|后端代码]]') }}</div>
+    <Editor v-if="coding" v-model="codes[coding]" class="h-full" />
+  </div>
+  <Transition name="fade">
+    <div class="fixed top-0 left-0 w-screen h-screen bg-black opacity-50" v-if="showForm" @click="showForm = false" />
+  </Transition>
+  <div class="all-transition fixed w-11/12 md:w-1/3 bg-white h-screen" :class="showForm ? 'right-0' : '-right-full'">
+    <h4 class="text-lg font-bold text-center my-2">{{ I('[[All forms|所有表单]]') }}</h4>
+    <input class="w-full py-1 px-2 mb-2 text-sm font-mono border" :placeholder="I('[[Search Form ID|搜索表单ID]]')" v-model="keyword">
+    <div v-for="n in state.nodes">
+      <div v-if="n.data.type === 'form'" class="flex items-center px-1 cursor-pointer all-transition" :class="forms[n.node] ? 'bg-blue-200' : 'bg-gray-100'" @click="choose(n.node)">
+        <CheckIcon v-if="forms[n.node]" class="w-4 text-blue-500" />
+        <PlusIcon v-else class="w-4 text-gray-400" />
+        <div class="mx-1 font-bold">{{ n.data.title }}</div>
+      </div>
+    </div>
+  </div>
   <div class="w-full min-h-full p-4" v-if="info.time">
     <h3 class="font-bold flex items-center justify-between my-1">
-      <input class="bg-transparent text-xl" v-model="info.title" :readonly="!editable">
-      <button v-if="editable" @click="submit" class="bg-blue-500 rounded shadow all-transition hover:shadow-md px-3 py-1 text-sm text-white">{{ I('[[Save|保存]]') }}</button>
+      <input class="bg-transparent text-xl" v-model="info.title">
+      <button @click="submit" class="bg-blue-500 rounded shadow all-transition hover:shadow-md px-3 py-1 text-sm text-white">{{ I('[[Save|保存]]') }}</button>
     </h3>
+    <div class="flex flex-col md:flex-row w-full">
+      <div class="p-3 m-2 bg-white shadow rounded grow">
+        <h3 class="text-lg font-bold border-b">{{ I('[[Service Forms|服务表单]]') }}</h3>
+        <div v-for="f, id in forms" class="all-transition border-b border-gray-200 hover:bg-gray-100 bg-white p-2 text-gray-700 flex">
+          <div>{{ f.title }}</div>
+          <div class="grow"></div>
+          <CodeBracketIcon class="w-5 mx-2 rounded cursor-pointer text-green-500" @click="coding = id" />
+          <XMarkIcon class="w-5 mx-2 rounded cursor-pointer text-red-500" @click="delete forms[id]"/>
+        </div>
+        <button class="flex items-center px-3 py-1 bg-blue-500 rounded text-white font-bold my-2" @click="showForm = true">
+          <PlusIcon class="w-6 mr-1"/>
+          {{ I('[[Add Form|添加表单]]') }}
+        </button>
+      </div>
+      <div class="grow">
+        <div class="p-3 m-2 bg-white shadow rounded">
+          something else
+        </div>
+      </div>
+    </div>
   </div>
 </template>
