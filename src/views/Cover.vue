450c1aeda93cbab1202f21fe7d61f15b08fe970c
VampireRabbit <1662025808@qq.com>
2023-01-02 21:23:43 +0800

    Create new branch

--- src/views/Cover.vue
+++ src/views/Cover.vue
@@ -2,8 +2,28 @@
 </script>
 
 <template>
-  <div class="p-4">
-    <h1 class="font-bold text-2xl">New Atool!</h1>
-    <p>The new Atool is still under construction</p>
+  <div class="flex flex-col bg-white items-center p-4">
+    <div class="flex justify-around border-b-4 p-4">
+      <img src="src/assets/online.svg" class="p-6">
+      <div class="flex flex-col items-center p-6">
+        <p class="font-bold p-8 my-10 text-4xl">Welcome to New Atool!</p>
+        <p class="p-4 text-2xl">The new Atool is still under construction</p>
+      </div>
+    </div>
+    <p class="text-3xl font-serif p-4 mt-6">An Amazing Tool</p>
+    <div class="flex w-screen justify-around p-8">
+      <div class="flex flex-col w-1/3 items-center p-6">
+        <img src="src/assets/calculator.svg" class="p-4">
+        <p class="text-xl p-4">Powerful Calculator</p>
+      </div>
+      <div class="flex flex-col w-1/3 items-center p-6">
+        <img src="src/assets/website.svg" class="p-4">
+        <p class="text-xl p-4">Flexible Interaction</p>
+      </div>
+      <div class="flex flex-col w-1/3 items-center p-6">
+        <img src="src/assets/documents.svg" class="p-4">
+        <p class="text-xl p-4">Designable Questionare</p>
+      </div>
+    </div>
   </div>
 </template>
