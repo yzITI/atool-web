b1eb0b65c2342d0dee297f6fdffa9539ec20273a
Phantomlsh <phantomlsh@outlook.com>
2023-10-14 13:43:32 -0500

    Update Select.vue

--- src/blocks/Select.vue
+++ src/blocks/Select.vue
@@ -6,7 +6,7 @@ let block = $computed(() => props.form[props.i])
 <template>
   <label>
     <span :class="block.multiple ? 'block' : 'inline'">{{ block.label }}</span>
-    <select class="border rounded text-left" :class="block.multiple ? 'overflow-auto w-full my-2' : 'mx-2 px-2 py-1'" v-model="props.state[block.key]" :multiple="block.multiple" :size="block.options.length">
+    <select class="border rounded text-left" :class="block.multiple ? 'overflow-auto w-full my-2' : 'mx-2 px-2 py-1'" v-model="props.state[block.key]" :multiple="block.multiple" :size="block.multiple ? block.options.length : 1">
       <option v-for="item in block.options">{{ item }}</option>
     </select>
   </label>
